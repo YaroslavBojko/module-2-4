dfb9486a9fac9c2484b874515d310134bc426563
Yaroslav Boyko <yaroslav.bojko.82@bk.ru>
2023-11-17 20:35:06 +0300

    finish

--- main.cpp
+++ main.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
 
-int main() {
+int main()
+{
+    int sum = 4000000;
+    int entrances = 10;
+    int apartments = 40;
+    int pay = sum / (entrances * apartments);
+
     std::cout << "Welcome to the rent calculator!" << std::endl;
     std::cout << "-------------------------------" << std::endl;
-    std::cout << "Enter the amount indicated on the receipt: 4000000" << std::endl;
-    std::cout << "How many entrances are there in your house? 10" << std::endl;
-    std::cout << "How many apartments are there in each entrance? 40" << std::endl;
+    std::cout << "Enter the amount indicated on the receipt: " << sum  << std::endl;
+    std::cout << "How many entrances are there in your house? " << entrances << std::endl;
+    std::cout << "How many apartments are there in each entrance? " << apartments << std::endl;
     std::cout << "--------------We count---------------" << std::endl;
-    std::cout << "Each apartment must pay 10,000 rubles." << std::endl;
+    std::cout << "Each apartment must pay " << pay << " rubles." << std::endl;
 }
