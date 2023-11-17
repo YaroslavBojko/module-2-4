d8001c0a8ab2ee812e64fda8165dbbbd0416ad36
Yaroslav Boyko <yaroslav.bojko.82@bk.ru>
2023-11-17 20:26:29 +0300

    text

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    std::cout << "Welcome to the rent calculator!" << std::endl;
+    std::cout << "-------------------------------" << std::endl;
+    std::cout << "Enter the amount indicated on the receipt: 4000000" << std::endl;
+    std::cout << "How many entrances are there in your house? 10" << std::endl;
+    std::cout << "How many apartments are there in each entrance? 40" << std::endl;
+    std::cout << "--------------We count---------------" << std::endl;
+    std::cout << "Each apartment must pay 10,000 rubles." << std::endl;
 }
